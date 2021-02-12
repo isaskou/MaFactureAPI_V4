8ee1d1c86ea495c20c318c492a50e7941ec81be7
isaskou <isabel.skou@yahoo.fr>
2021-02-12 12:22:00 +0100

    update Database with Client

---
+++ MaFacture2.Database/StoredProcedure/Client/SP-GetAllClientWithContact.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[SP-GetAllClientWithContact]
+	
+AS
+	SELECT cl.ClientId, co.Prenom, co.Nom, co.Surnom, co.Tel, co.Email, co.Rue, co.Num, co.CodePostal, co.Ville, co.Pays, cl.NomSociete, cl.NumTVA
+	FROM Client cl, Contact co
+	WHERE co.ContactId=cl.ContactId
+RETURN 0
